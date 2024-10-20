3cc06f6f95ff788479c97215fd596771cecfe30c
danbi <leedanbiii@naver.com>
2024-10-20 18:14:53 +0900

    post endpoint 통합 테스트

---
+++ src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
@@ -0,0 +1,36 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.dto.CourseDTO
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.assertTrue
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CourseControllerIntgTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun addCourse() {
+        val courseDTO = CourseDTO(null, "Build Restful APIs using SpringBoot and Kotlin", "Danbi")
+
+        val savedCourseDTO = webTestClient
+            .post() // post 요청
+            .uri("/v1/courses") // 요청 보낼 uri
+            .bodyValue(courseDTO) // 요청 본문에 courseDTO 객체 설정
+            .exchange() // 요청을 서버로 전송하고 응답 받음
+            .expectStatus().isCreated // 응답 상태 201 created 확인
+            .expectBody(CourseDTO::class.java) // 응답 본문 CourseDTO 타입 역직렬화
+            .returnResult() // 테스트 결과 반환
+            .responseBody // 응답 본문 추출
+
+        assertTrue { savedCourseDTO!!.id != null }
+    }
+}
