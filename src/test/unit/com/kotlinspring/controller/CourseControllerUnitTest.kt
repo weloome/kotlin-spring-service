7072fd3cc876b8496ce073477f54e20f84b33dbf
danbi <leedanbiii@naver.com>
2024-10-20 22:35:42 +0900

    put endpoint 유닛 테스트

--- src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
+++ src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
@@ -1,10 +1,12 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.entity.Course
 import com.kotlinspring.service.CourseService
 import com.kotlinspring.util.courseDTO
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
@@ -61,5 +63,33 @@ class CourseControllerUnitTest {
 
         println("courseDTOs : $courseDTO")
         assertEquals(2, courseDTO!!.size)
+    }
+
+    @Test
+    fun updateCourse() {
+        //existing course
+        val course = Course(null,
+            "Build RestFul APIs using SpringBoot and Kotlin",
+            "Development")
+        every { courseServiceMockk.updateCourse(any(), any()) } returns courseDTO(id=100,
+            name = "Build Restful APIs using SpringBoot and Kotlin1")
+
+        //courseId
+        //updated courseId
+        val updatedCourseDTO = CourseDTO(null,
+            "Build Restful APIs using SpringBoot and Kotlin1",
+            "Development")
+
+        val updatedCourse = webTestClient
+            .put()
+            .uri("/v1/courses/{courseId}", 100)
+            .bodyValue(updatedCourseDTO)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertEquals("Build Restful APIs using SpringBoot and Kotlin1", updatedCourse!!.name)
     }
 }
