4933917337bdd3106ae9a6cee55cdb16dd324073
danbi <leedanbiii@naver.com>
2024-10-20 14:51:18 +0900

    profile setup

--- src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
+++ src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
@@ -1,9 +1,13 @@
 package com.kotlinspring.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingsService {
 
-    fun retrieveGreeting(name: String) = "Hello $name"
+    @Value("\${message}")
+    lateinit var message: String
+
+    fun retrieveGreeting(name: String) = "$name, $message"
 }
