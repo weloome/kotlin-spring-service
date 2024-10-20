b27e42463a1f5670d77f38c2ec2a4651d98b24da
danbi <leedanbiii@naver.com>
2024-10-20 19:15:04 +0900

    get endpoint 통합테스트

--- src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
+++ src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
@@ -1,12 +1,16 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.repository.CourseRepository
+import com.kotlinspring.util.courseEntityList
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,6 +20,16 @@ class CourseControllerIntgTest {
 
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
 
     @Test
     fun addCourse() {
@@ -32,5 +46,20 @@ class CourseControllerIntgTest {
             .responseBody // 응답 본문 추출
 
         assertTrue { savedCourseDTO!!.id != null }
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+        val courseDTO = webTestClient
+            .get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        println("courseDTOs : $courseDTO")
+        assertEquals(3, courseDTO!!.size)
     }
 }
