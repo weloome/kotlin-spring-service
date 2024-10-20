bd73b83bc5a0b695cf1dafa5ecfeb3905779ffa2
danbi <leedanbiii@naver.com>
2024-10-20 14:56:52 +0900

    logger

--- src/main/kotlin/com/kotlinspring/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlinspring/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.service.GreetingsService
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/greetings")
 class GreetingController(val greetingsService: GreetingsService) {
 
+    companion object : KLogging()
+
     @GetMapping("/{name}")
     fun retrieveGreeting(@PathVariable("name") name: String): String {
+        logger.info("Name is $name")
         return greetingsService.retrieveGreeting(name)
     }
 }
