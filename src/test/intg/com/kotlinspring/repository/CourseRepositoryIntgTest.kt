b7acfd2a6a47609760a71faa16b9826bfc3d4266
leedanbi <leedanbiii@gentlemonster.com>
2024-10-25 10:36:46 +0900

    @ParameterizedTest

--- src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
+++ src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
@@ -1,12 +1,17 @@
 package com.kotlinspring.repository
 
 import com.kotlinspring.util.courseEntityList
+import jakarta.validation.constraints.Size
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -34,5 +39,23 @@ class CourseRepositoryIntgTest {
         val courses = courseRepository.findCoursesByName("SpringBoot")
         println("coursere: $courses")
         Assertions.assertEquals(2, courses.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(name: String, expectedSize: Int) {
+        val courses = courseRepository.findCoursesByName(name)
+        println("coursere: $courses")
+        Assertions.assertEquals(expectedSize, courses.size)
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1)
+            )
+        }
     }
 }
