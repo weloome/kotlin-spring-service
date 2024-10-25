bd165976ac1e1a4b693566a3b1e0f403e39b301c
leedanbi <leedanbiii@gentlemonster.com>
2024-10-25 11:42:01 +0900

    createInstructor api

---
+++ src/main/kotlin/com/kotlinspring/controller/InstructorController.kt
@@ -0,0 +1,21 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.dto.InstructorDTO
+import com.kotlinspring.service.InstructorService
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/instructors")
+@Validated
+class InstructorController(val instructorService: InstructorService) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createInstructor(@RequestBody instructorDTO: InstructorDTO) = instructorService.createInstructor(instructorDTO)
+}
